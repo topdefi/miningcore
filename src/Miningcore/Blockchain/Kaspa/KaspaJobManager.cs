cb7ddc4590c4b5f6b1c61a808ecbe64be374df65
topdefi <94203269+topdefi@users.noreply.github.com>
2024-05-24 12:31:10 -0700

    Updates

--- src/Miningcore/Blockchain/Kaspa/KaspaJobManager.cs
+++ src/Miningcore/Blockchain/Kaspa/KaspaJobManager.cs
@@ -751,7 +751,7 @@ public class KaspaJobManager : JobManagerBase<KaspaJob>
         extraPoolPaymentProcessingConfig = pc.PaymentProcessing.Extra.SafeExtensionDataAs<KaspaPaymentProcessingConfigExtra>();
         
         maxActiveJobs = extraPoolConfig?.MaxActiveJobs ?? 8;
-        extraData = extraPoolConfig?.ExtraData ?? "Miningcore.developers[\"Cedric CRISPIN\"]";
+        extraData = extraPoolConfig?.ExtraData ?? "Miningcore.developers[\"Core Mining\"]";
         
         // extract standard daemon endpoints
         daemonEndpoints = pc.Daemons
